19898e732404c522ac839066b7c53e8e38d81834
Heidi Klem <heidi.klem@colostate.edu>
2019-03-11 13:22:00 -0600

    added adjaceny graph script

--- 1D_ENM/plotting.py
+++ 1D_ENM/plotting.py
@@ -1,147 +1,94 @@
-import numpy as np
+
 import matplotlib.pyplot as plt
+from matplotlib.ticker import MultipleLocator
+import matplotlib.cm as cm
+import numpy as np
 
-Nstep = 2000001 
-Nwrite= 2000
+# define function for making figures
+def define_figure(xlabel="X",ylabel="Y"):
+    # setup plot parameters
+    fig = plt.figure(figsize=(10,8), dpi= 80, facecolor='w', edgecolor='k')
+    ax = plt.subplot(111)
+    ax.grid(b=True, which='major', axis='both', color='#808080', linestyle='--')
+    ax.set_xlabel('Nodes',size=20)
+    ax.set_ylabel('Nodes',size=20)
+    plt.tick_params(axis='both',labelsize=20)
+    return ax
 
+def plot_square_matrix(square_matrix,figure_name,axes_label='',cbar_label='',plotting_cmap='bwr',v_range=None,minor_ticks=10,major_ticks=100):
+        """
+        """
+        nNodes = len(square_matrix)
+        node_range = range(nNodes+1)
+        fig, ax = plt.subplots()
+        ax.tick_params(which='major',length=6,width=2)
+        ax.tick_params(which='minor',length=3,width=1)
+        ax.xaxis.set_minor_locator(MultipleLocator(minor_ticks))
+        ax.xaxis.set_major_locator(MultipleLocator(major_ticks))
+        ax.yaxis.set_minor_locator(MultipleLocator(minor_ticks))
+        ax.yaxis.set_major_locator(MultipleLocator(major_ticks))
 
-# plot
-data = np.loadtxt("1D_allosterysim.dat")
+        if v_range != None:
+                temp = plt.pcolormesh(node_range,node_range,square_matrix,cmap=plotting_cmap,vmin=v_range[0],vmax=v_range[1])
+        else:
+                temp = plt.pcolormesh(node_range,node_range,square_matrix,cmap=plotting_cmap)
+        cb1 = plt.colorbar()
+        cb1.set_label(r'%s'%(cbar_label))
 
-pos = np.loadtxt("positions.dat")
-nSteps = len(pos)
-nSteps_range = range(nSteps)
+        xlabels = [str(int(x)+1) for x in temp.axes.get_xticks()[:]]
+        ylabels = [str(int(y)+1) for y in temp.axes.get_yticks()[:]]
+        temp.axes.set_xticks(temp.axes.get_xticks(minor=True)[:]+0.5,minor=True)
+        temp.axes.set_xticks(temp.axes.get_xticks()[:]+0.5)
+        temp.axes.set_yticks(temp.axes.get_yticks(minor=True)[:]+0.5,minor=True)
+        temp.axes.set_yticks(temp.axes.get_yticks()[:]+0.5)
+        temp.axes.set_xticklabels(xlabels)
+        temp.axes.set_yticklabels(ylabels)
 
-t = np.array(range(int((Nstep-1)/Nwrite)))*2
+        plt.xlim((0,nNodes))
+        plt.ylim((0,nNodes))
+        plt.xlabel(axes_label,size=14)
+        plt.ylabel(axes_label,size=14)
+        ax.set_aspect('equal')
+        plt.tight_layout()
+        plt.plot(np.arange(0,nNodes,0.01),np.arange(0,nNodes,0.01),'--',lw=2,color='k')
+        plt.savefig(figure_name,dpi=600,transparent=True)
+        plt.close()
 
-plt.plot(t,pos[:,0],label='Particle 1')
-plt.plot(t,pos[:,1],label='Particle 2')
-plt.plot(t,pos[:,2],label='Particle 3')
-plt.plot(t,pos[:,3],label='Particle 4')
-plt.plot(t,pos[:,4],label='Particle 5')
-plt.plot(t,pos[:,5],label='Particle 6')
-plt.plot(t,pos[:,6],label='Particle 7')
-plt.plot(t,pos[:,7],label='Particle 8')
-plt.plot(t,pos[:,8],label='Particle 9')
+def plot_square_matrix_inline(square_matrix,figure_name,axes_label='',cbar_label='',plotting_cmap='bwr',v_range=None,minor_ticks=10,major_ticks=100):
+        """
+        """
+        nNodes = len(square_matrix)
+        node_range = range(nNodes+1)
+        fig, ax = plt.subplots()
+        ax.tick_params(which='major',length=6,width=2)
+        ax.tick_params(which='minor',length=3,width=1)
+        ax.xaxis.set_minor_locator(MultipleLocator(minor_ticks))
+        ax.xaxis.set_major_locator(MultipleLocator(major_ticks))
+        ax.yaxis.set_minor_locator(MultipleLocator(minor_ticks))
+        ax.yaxis.set_major_locator(MultipleLocator(major_ticks))
 
-plt.xlabel("Femtoseconds",size=14)
-plt.ylabel("Angstroms",size=14)
-plt.legend(loc='upper left')
-plt.title("Positions vs. Time")
-plt.grid()
-plt.tight_layout()
+        if v_range != None:
+                temp = plt.pcolormesh(node_range,node_range,square_matrix,cmap=plotting_cmap,vmin=v_range[0],vmax=v_range[1])
+        else:
+                temp = plt.pcolormesh(node_range,node_range,square_matrix,cmap=plotting_cmap)
+        cb1 = plt.colorbar()
+        cb1.set_label(r'%s'%(cbar_label))
 
-plt.savefig("9N_positionvstime_w_centerofgeom.pdf",dpi=600,transparant=True)
-plt.close()
+        xlabels = [str(int(x)+1) for x in temp.axes.get_xticks()[:]]
+        ylabels = [str(int(y)+1) for y in temp.axes.get_yticks()[:]]
+        temp.axes.set_xticks(temp.axes.get_xticks(minor=True)[:]+0.5,minor=True)
+        temp.axes.set_xticks(temp.axes.get_xticks()[:]+0.5)
+        temp.axes.set_yticks(temp.axes.get_yticks(minor=True)[:]+0.5,minor=True)
+        temp.axes.set_yticks(temp.axes.get_yticks()[:]+0.5)
+        temp.axes.set_xticklabels(xlabels)
+        temp.axes.set_yticklabels(ylabels)
 
+        plt.xlim((0,nNodes))
+        plt.ylim((0,nNodes))
+        plt.xlabel(axes_label,size=14)
+        plt.ylabel(axes_label,size=14)
+        ax.set_aspect('equal')
+        plt.tight_layout()
+        plt.plot(np.arange(0,nNodes,0.01),np.arange(0,nNodes,0.01),'--',lw=2,color='k')
 
-# center of geometry motion
-for ts in nSteps_range:
-    center_of_geometry = np.mean(pos[ts])
-    pos[ts] -= center_of_geometry
-plt.hist(pos[:,0],histtype='barstacked',label='Particle 1',bins=500)
-plt.hist(pos[:,1],histtype='step',label='Particle 2',bins=500)
-plt.hist(pos[:,2],histtype='barstacked',label='Particle 3',bins=500)
-plt.hist(pos[:,3],histtype='barstacked',label='Particle 4',bins=500)
-plt.hist(pos[:,4],histtype='step',label='Particle 5',bins=500)
-plt.hist(pos[:,5],histtype='barstacked',label='Particle 6',bins=500)
-plt.hist(pos[:,6],histtype='barstacked',label='Particle 7',bins=500)
-plt.hist(pos[:,7],histtype='step',label='Particle 8',bins=500)
-plt.hist(pos[:,8],histtype='barstacked',label='Particle 9',bins=500)
-
-plt.xlabel("Positions",size=14)
-plt.ylabel("Counts",size=14)
-plt.legend(loc='upper left')
-plt.title("Position")
-plt.grid()
-plt.tight_layout()
-
-plt.savefig("9N_positionhist.pdf",dpi=600,transparant=True)
-plt.close()
-
-plt.hist(data[:,0], histtype='step',label='Particle 1',bins=500)
-plt.hist(data[:,3], histtype='step',label='Particle 2',bins=500)
-plt.hist(data[:,6], histtype='step',label='Particle 3',bins=500)
-plt.hist(data[:,9], histtype='step',label='Particle 4',bins=500)
-plt.hist(data[:,12], histtype='step',label='Particle 5',bins=500)
-plt.hist(data[:,15], histtype='step',label='Particle 6',bins=500)
-plt.hist(data[:,18], histtype='step',label='Particle 7',bins=500)
-plt.hist(data[:,21], histtype='step',label='Particle 8',bins=500)
-plt.hist(data[:,24], histtype='step',label='Particle 9',bins=500)
-
-plt.xlabel("Forces",size=14)
-plt.ylabel("Counts",size=14)
-plt.legend(loc='upper left')
-plt.title("Forces")
-plt.grid()
-plt.tight_layout()
-
-plt.savefig("9N_forceshist.pdf",dpi=600,transparant=True)
-plt.close()
-
-plt.hist(data[500:,2], histtype='step',label='Particle 1',bins=50)
-plt.hist(data[500:,5], histtype='step',label='Particle 2',bins=50)
-plt.hist(data[500:,8], histtype='step',label='Particle 3',bins=50)
-plt.hist(data[500:,11], histtype='step',label='Particle 4',bins=50)
-plt.hist(data[500:,14], histtype='step',label='Particle 5',bins=50)
-plt.hist(data[500:,17], histtype='step',label='Particle 6',bins=50)
-plt.hist(data[500:,20], histtype='step',label='Particle 7',bins=50)
-plt.hist(data[500:,23], histtype='step',label='Particle 8',bins=50)
-plt.hist(data[500:,26], histtype='step',label='Particle 9',bins=50)
-
-plt.xlabel("Velocities",size=14)
-plt.ylabel("Counts",size=14)
-plt.legend(loc='upper left')
-plt.title("Velocities")
-plt.grid()
-plt.tight_layout()
-
-plt.savefig("9N_velocitieshist.pdf",dpi=600,transparant=True)
-plt.close()
-
-
-t = np.array(range(int((Nstep-1)/Nwrite)))*2
-
-plt.plot(t,pos[:,0],label='Particle 1')
-plt.plot(t,pos[:,1],label='Particle 2')
-plt.plot(t,pos[:,2],label='Particle 3')
-plt.plot(t,pos[:,3],label='Particle 4')
-plt.plot(t,pos[:,4],label='Particle 5')
-plt.plot(t,pos[:,5],label='Particle 6')
-plt.plot(t,pos[:,6],label='Particle 7')
-plt.plot(t,pos[:,7],label='Particle 8')
-plt.plot(t,pos[:,8],label='Particle 9')
-
-plt.ylim(-10,10)
-plt.xlabel("Femtoseconds",size=14)
-plt.ylabel("Angstroms",size=14)
-plt.legend(loc='upper left')
-plt.title("Positions vs. Time")
-plt.grid()
-plt.tight_layout()
-
-plt.savefig("9N_positionvstime.pdf",dpi=600,transparant=True)
-plt.close()
-
-t = np.array(range(int((Nstep-1)/Nwrite)-500))*2
-
-plt.plot(t,data[500:,2],label='Particle 1')
-plt.plot(t,data[500:,5],label='Particle 2')
-plt.plot(t,data[500:,8],label='Particle 3')
-plt.plot(t,data[500:,11],label='Particle 4')
-plt.plot(t,data[500:,14],label='Particle 5')
-plt.plot(t,data[500:,17],label='Particle 6')
-plt.plot(t,data[500:,20],label='Particle 7')
-plt.plot(t,data[500:,23],label='Particle 8')
-plt.plot(t,data[500:,26],label='Particle 9')
-
-plt.xlabel("Femtoseconds",size=14)
-plt.ylabel("Velocity",size=14)
-plt.legend(loc='upper left')
-plt.title("Velocity vs. Time")
-plt.grid()
-plt.tight_layout()
-
-plt.savefig("9N_velocitiesvstime.pdf",dpi=600,transparant=True)
-plt.close()
 
